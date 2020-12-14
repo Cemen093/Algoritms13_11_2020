1cba1e092efae1d9946f18427f7dbb122ceb9793
Cemen093 <CemenP93@gmail.com>
2020-12-14 18:13:42 +0200

    I read DZ, now ready

--- DzRedBlackTree/src/org/itstep/Main.java
+++ DzRedBlackTree/src/org/itstep/Main.java
@@ -7,12 +7,17 @@ public class Main {
         Tree tree = new Tree();
         Scanner scanner = new Scanner(System.in);
 
-        int[] arr = {100, 80, 120, 70, 90, 85, 95, 83,84};
-
-        for (int i = 0; i < arr.length; i++) {
-            tree.add(arr[i], i);
+//        int[] arr = {100, 80, 120, 70, 90, 85, 95, 83,84};
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            tree.add(arr[i], i);
+//        }
+        System.out.print("How many elements do you want to add to the tree? > ");
+        int numInputNode = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < numInputNode; i++) {
+            System.out.print("Input key and inf > ");
+            tree.add(scanner.nextShort(), scanner.nextShort());
         }
-
         System.out.println(tree.toString());
     }
 }
